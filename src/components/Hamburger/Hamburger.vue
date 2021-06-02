924ada5df600c0fb2b2c1a937f621d2258dab0ef
xiyang.bai <xiyang.bai@zhangmen.com>
2021-06-02 21:19:21 +0800

    style(styles): style(other): feat style-eslint

--- src/components/Hamburger/Hamburger.vue
+++ src/components/Hamburger/Hamburger.vue
@@ -38,12 +38,12 @@ export default defineComponent({
 })
 </script>
 <style lang="stylus" scoped>
-.hamburger {
+.hamburger
   display: inline-block;
   vertical-align: middle;
   width: 20px;
   height: 20px;
-}
+
 
 .hamburger.is-active {
   transform: rotate(180deg);
