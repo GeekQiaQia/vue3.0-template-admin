132533e3c6c0b86eecfb7f1234b2ac34ee270f49
xiyang.bai <xiyang.bai@zhangmen.com>
2021-06-29 15:50:32 +0800

    feat(components): init search

---
+++ src/components/Search/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <el-select v-model="value" placeholder="请选择">
+    <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"> </el-option>
+  </el-select>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {}
+})
+</script>
+<style lang="stylus" scoped></style>
