faf51441930432fbe1e594659a97f5d1c976c973
GeekQiaQia <xiyangbai@outlook.com>
2021-07-27 02:14:44 +0000

    Deploying to gh-pages from  @ 27adec7d40534dba6a93c1b6df4b793515b05862 🚀

--- assets/index.07fc2e36.js
+++
@@ -1,1 +0,0 @@
-import{r as s}from"./index.25816aae.js";class e{static postSetBasicInfo(e){return s({url:"/api/setting/basicInfo",method:"post",json:!0,data:e}).then((s=>0===s.status?Promise.resolve(s):Promise.reject(s)))}static getPersonTags(){return s({url:"/api/personal/tags",method:"get",json:!0}).then((s=>0===s.status?Promise.resolve(s):Promise.reject(s)))}}export{e as S};
