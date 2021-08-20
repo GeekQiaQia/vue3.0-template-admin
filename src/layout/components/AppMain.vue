5f3bb39f79b8738fd629e88016d126fca2238e50
xiyang.bai <xiyang.bai@zhangmen.com>
2021-08-20 14:53:47 +0800

    build(deps): update vue@next vite vue-router to latest

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -4,14 +4,14 @@
       <el-tab-pane v-for="item in tabsOption" :key="item.route" :closable="item.route !== '/home'" :label="item.title[lang]" :name="item.route">
         <router-view v-if="$route.meta.keepAlive" v-slot="{ Component }">
           <transition name="fade" mode="out-in">
-            <component :is="Component" />
+            <component :is="Component" :key="$route.path" />
           </transition>
         </router-view>
 
         <router-view v-if="!$route.meta.keepAlive" v-slot="{ Component }">
           <transition name="fade" mode="out-in">
             <keep-alive>
-              <component :is="Component" />
+              <component :is="Component" :key="$route.path" />
             </keep-alive>
           </transition>
         </router-view>
