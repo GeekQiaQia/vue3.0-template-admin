7ac7041545e3f78411aed2ffbe91688965d84d70
xiyang.bai <xiyang.bai@zhangmen.com>
2021-08-26 16:00:13 +0800

    fix(element-ui): theme style some bug in new version

--- src/hooks/useFiles.js
+++ src/hooks/useFiles.js
@@ -2,19 +2,19 @@ import { useFetch } from './useFetch'
 
 export const useFiles = () => {
   const getFontFiles = async (fontFiles) => {
-    const fonts = await Promise.all(fontFiles.map(font => useFetch(`//unpkg.com/element-plus/lib/theme-chalk/fonts/${font}`, true)))
+    const fonts = await Promise.all(fontFiles.map(font => useFetch(`//unpkg.com/element-plus/packages/theme-chalk/fonts/${font}`, true)))
     return fonts
   }
 
   const getIndexStyle = async () => {
-    const { data } = await useFetch('//unpkg.com/element-plus/lib/theme-chalk/index.css')
+    const { data } = await useFetch('//unpkg.com/element-plus/dist/index.css')
     return data
   }
 
   const getSeparatedStyles = async () => {
-    const {data} = await useFetch('//unpkg.com/element-plus/lib/theme-chalk/')
+    const {data} = await useFetch('//unpkg.com/element-plus/packages/theme-chalk/')
     const styles = data.match(/href="[\w-]+\.css"/g).map(str => str.split('"')[1])
-    const files = await Promise.all(styles.map(file => useFetch(`//unpkg.com/element-plus/lib/theme-chalk/${file}`)))
+    const files = await Promise.all(styles.map(file => useFetch(`//unpkg.com/element-plus/packages/theme-chalk/${file}`)))
     return files
   }
 
