1ddd4d36809157ab9b7e538033ad117937ed418c
wangchunsen <xiaoguiseni@163.com>
2021-10-23 12:51:01 +0800

    config

--- vite.config.ts
+++ vite.config.ts
@@ -56,11 +56,21 @@ export default defineConfig({
   },
    // https://www.vitejs.net/config/#build-commonjsoptions
    build:{
+    chunkSizeWarningLimit: 1500,
     commonjsOptions:{
       ignoreDynamicRequires:false, // Default: false
       transformMixedEsModules:true,
       brotliSize:false, // 禁用会提高大型项目的构建性能。
       sourceMap:false
+    },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if(id.includes('node_modules')) {
+            return id.toString().split('node_modules')[1].split('/')[0].toString()
+          }
+        }
+      }
     }
   },
   optimizeDeps: {
