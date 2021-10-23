d420432bb1a18c18c1b89e66db888228008c22f7
wangchunsen <xiaoguiseni@163.com>
2021-10-23 21:30:25 +0800

    more chunk size

--- vite.config.ts
+++ vite.config.ts
@@ -56,7 +56,7 @@ export default defineConfig({
   },
    // https://www.vitejs.net/config/#build-commonjsoptions
    build:{
-    chunkSizeWarningLimit: 1500,
+    chunkSizeWarningLimit: 2000,
     commonjsOptions:{
       ignoreDynamicRequires:false, // Default: false
       transformMixedEsModules:true,
@@ -67,7 +67,7 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if(id.includes('node_modules')) {
-            return id.toString().split('node_modules')[1].split('/')[0].toString()
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
           }
         }
       }
