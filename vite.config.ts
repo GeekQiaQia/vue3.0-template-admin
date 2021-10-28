4b198c93eb3e88c4f05c50d242b8a69aee205995
Gavin <1490259042@qq.com>
2021-10-28 11:26:51 +0800

    chore: 注释分包配置

--- vite.config.ts
+++ vite.config.ts
@@ -62,15 +62,15 @@ export default defineConfig({
       transformMixedEsModules: true,
       sourceMap: false
     },
-    rollupOptions: {
-      output: {
-        manualChunks(id) {  // https://rollupjs.org/guide/en/#outputmanualchunks
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString()
-          }
-        }
-      }
-    }
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks(id) {  // https://rollupjs.org/guide/en/#outputmanualchunks
+    //       if (id.includes('node_modules')) {
+    //         return id.toString().split('node_modules/')[1].split('/')[0].toString()
+    //       }
+    //     }
+    //   }
+    // }
   },
   optimizeDeps: {
     include: [
