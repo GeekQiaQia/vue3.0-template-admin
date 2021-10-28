6e80fe94b77d8f3e1e0c18600aec302fe31d9a3a
Gavin <1490259042@qq.com>
2021-10-28 23:51:58 +0800

    feat: 初始化列表展示也

---
+++ src/views/projectBoard/task-column.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="task-column">
+    <div ref="column">
+      <template v-for="item in dataList">
+        <!-- content -->
+      </template>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import _ from 'lodash'
+import { ref, Ref } from 'vue'
+
+defineProps()
+
+const emit = defineEmits()
+
+const column: Ref<HTMLDivElement> = ref()
+
+const dataList = ref([])
+
+</script>
+
+<style lang="stylus" scoped>
+.task-column {
+
+}
+</style>
