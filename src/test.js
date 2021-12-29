b6d80d8cbc1e8f1cc5508e7e58986f901fa8c3a9
geekqiaqia <xiyangbai@outlook.com>
2021-12-29 14:54:06 +0800

    fix(hooks): fix(hooks): test pre commit

---
+++ src/test.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: GeekQiaQia
+ * @Date: 2021-12-29 14:53:16
+ * @LastEditTime: 2021-12-29 14:53:17
+ * @LastEditors: GeekQiaQia
+ * @Description:
+ * @FilePath: /vue3.0-template-admin/src/test.js
+ */
+
+
+console.log('test');
