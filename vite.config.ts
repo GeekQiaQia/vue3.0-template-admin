28c3d48e122b2c6c1184f6cb8863266e53e6e7c0
shaun <xybai@telenavsoftware.com>
2022-07-28 15:23:02 +0800

    docs(other): update readme doc &&  vite config

--- vite.config.ts
+++ vite.config.ts
@@ -10,14 +10,6 @@ import path from 'path'
 import { ConfigEnv, loadEnv, UserConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import legacy from '@vitejs/plugin-legacy'
-// Tips 去掉分包按需引入的方式、开发体验不好；您可根据需要体验按需引入方式；
-// import AutoImport from 'unplugin-auto-import/vite'
-// import Components from 'unplugin-vue-components/vite'
-// import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-// import OptimizationPersist from 'vite-plugin-optimize-persist'
-// import PkgConfig from 'vite-plugin-package-config'
-
-// const pathSrc = path.resolve(__dirname, 'src')
 
 const CWD = process.cwd()
 
@@ -48,21 +40,7 @@ export default ({ mode }: ConfigEnv): UserConfig => {
       }
     },
     plugins: [
-      // PkgConfig(),
-      // OptimizationPersist(),
       vue(),
-      // AutoImport({
-      //   //Tips 去掉分包按需引入的方式、开发体验不好；您可根据需要体验按需引入方式；
-      //   resolvers: [ElementPlusResolver()]
-      // }),
-      // Components({
-      //   resolvers: [
-      //     ElementPlusResolver({
-      //       importStyle: 'sass'
-      //     })
-      //   ],
-      //   dts: path.resolve(pathSrc, 'components.d.ts')
-      // }),
       legacy({
         targets: ['ie >= 11'],
         additionalLegacyPolyfills: ['regenerator-runtime/runtime']
